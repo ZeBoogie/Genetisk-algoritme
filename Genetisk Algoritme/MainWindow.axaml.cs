7144ba2b62f2bee63328764f44a88c713897b8f1
Jonathan <Jonathan.dyrekilde.sommerlund@gmail.com>
2023-01-16 09:06:44 +0100

    Project

---
+++ Genetisk Algoritme/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Genetisk_Algoritme
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
